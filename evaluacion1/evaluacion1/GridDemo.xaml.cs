2e309e55e06d5ec588290518946a3151d0b3b259
sabrina alexandra pomajulca razabal <lokita_sabri_14@hotmail.com>
2019-09-30 15:49:38 -0500

    first commit

---
+++ evaluacion1/evaluacion1/GridDemo.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace evaluacion1
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class GridDemo : ContentPage
+	{
+		public GridDemo ()
+		{
+			InitializeComponent ();
+            var grid = new Grid();
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(2, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(200) });
+            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(200) });
+
+        }
+    }
+}
---
+++ evaluacion1/evaluacion1/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace evaluacion1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Item1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new StackLayoutDemo());
+
+            };
+            Item2.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new GridDemo());
+            };
+            Item3.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new Page1());
+
+            };
+            Item7.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new TabbedPage1());
+
+            };
+            Item5.Clicked += async (sender, e) =>
+            {
+                //Call Popup
+                await Navigation.PushAsync(new DatepickerDemo());
+
+            };
+
+          
+
+        }
+    }
+}
